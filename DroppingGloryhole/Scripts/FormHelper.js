d4f66d3ff36d2c2eb09a0e90b17c3adb028cba11
DroppingScience <jonathanbspringer@gmail.com>
2018-05-22 14:14:02 -0500

    Groundwork for implementing custom form persistence

---
+++ DroppingGloryhole/Scripts/FormHelper.js
@@ -0,0 +1,22 @@
+﻿let subscribeEvents = function () {
+    $("input").on('change', function () {
+        let key = $(this).attr('id');
+        let value = $(this).val();
+        localStorage.setItem(key, value);
+    });
+    $(".incrementBtn").on('click', function () {
+        $(this).parent().find("input").val(function (i, oldVal) { return ++oldVal > 3 ? 3 : oldVal; });
+        $(this).parent().find("input").trigger("change");
+    });
+    $(".decrementBtn").on('click', function () {
+        $(this).parent().find("input").val(function (i, oldVal) { return --oldVal < 0 ? 0 : oldVal; });
+        $(this).parent().find("input").trigger("change");
+    });
+};
+let loadFormDataFromStorage = function () {
+    $("input").each(function () {
+        $(this).val(localStorage.getItem($(this).attr("id")));
+    });
+};
+$(document).ready(subscribeEvents);
+$(document).ready(loadFormDataFromStorage);
